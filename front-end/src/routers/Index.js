03c0fd72a50a1d25c46e48ec241810d81e361d5c
Chris Philbin <chris@chrisphilbin.net>
2021-09-21 07:31:33 -0400

    Creating login component and adding route

--- front-end/src/routers/Index.js
+++ front-end/src/routers/Index.js
@@ -4,12 +4,14 @@ import BingoCard from "../components/player/BingoCard";
 import Home from "../static/Home";
 import GameList from "../components/host/GameList";
 import NotFound from "../static/NotFound";
+import Login from "../static/Login";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/host/games" component={GameList} />
         <Route exact path="/play" component={BingoCard} />
         <Route component={NotFound} />
